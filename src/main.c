de59ad3a72a33ca41c781ee7d28bc9e9f7a10c96
Maxim LaPlante <maxim.laplante@maine.edu>
2025-05-02 08:28:23 -0400

    [jittery] potentiometer-controlled oscillator

--- src/main.c
+++ src/main.c
@@ -21,22 +21,37 @@
 
 #include "pico/stdlib.h"
 
-
-#define I2C_PORT i2c0
-#define I2C_SDA  4
-#define I2C_SCL  5
+#include "ece486_nco.h"
 
 #define LED_PIN 0
 
+#define I2C_SDA 4
+#define I2C_SCL 5
+#define I2C_PORT i2c0
+
 static __attribute__((aligned(8))) pio_i2s i2s; /****Not sure... **/
+NCO_T *osc1;
 
 volatile int32_t outsignal[STEREO_BUFFER_SIZE];
 
-static void process_audio(const int32_t* input, int32_t* output, size_t num_frames) {
-    // Just copy the input to the output
-    for (size_t i = 0; i < num_frames * 2; i++) {
-        output[i] = outsignal[i]; /** **CHANGED !!!! =input[i]*/
+static void process_audio(const int32_t* input, int32_t* output, size_t num_frames, NCO_T *osc) {
+    float f = (float)adc_read() * 0.00001f;
+    nco_set_frequency(osc, f);
+    nco_get_samples_dual_mono_int(osc, output, num_frames);
+    // left side
+    /*
+    for (size_t i = 0; i < num_frames; i++) {
+        output[2*i] = nco_get_samples(osc, );
+        // and right side...
     }
+     */
+
+    // right side
+    /*
+    for (size_t i = 0; i < num_frames; i++) {
+        output[2*i + 1] = outsignal[i];
+    }
+     /**/
 }
 
 static void dma_i2s_in_handler(void) {
@@ -46,10 +61,10 @@ static void dma_i2s_in_handler(void) {
      */
     if (*(int32_t**)dma_hw->ch[i2s.dma_ch_in_ctrl].read_addr == i2s.input_buffer) {
         // It is inputting to the second buffer so we can overwrite the first
-        process_audio(i2s.input_buffer, i2s.output_buffer, AUDIO_BUFFER_FRAMES);
+        process_audio(i2s.input_buffer, i2s.output_buffer, AUDIO_BUFFER_FRAMES, osc1);
     } else {
         // It is currently inputting the first buffer, so we write to the second
-        process_audio(&i2s.input_buffer[STEREO_BUFFER_SIZE], &i2s.output_buffer[STEREO_BUFFER_SIZE], AUDIO_BUFFER_FRAMES);
+        process_audio(&i2s.input_buffer[STEREO_BUFFER_SIZE], &i2s.output_buffer[STEREO_BUFFER_SIZE], AUDIO_BUFFER_FRAMES, osc1);
     }
     dma_hw->ints0 = 1u << i2s.dma_ch_in_data;  // clear the IRQ
 }
@@ -57,7 +72,7 @@ static void dma_i2s_in_handler(void) {
 
 // FROM I2S.C:      const i2s_config i2s_config_default = {48000, 256, 32, 10, 6, 7, 8, true};
 /** **BECAUSE THE DAC IS L/R, WE NEED TO SEND DATA AT TWICE THE SPEED. THIS BRINGS US FROM 44.1kHz to 88.2kHz */
-#define FS  100000 //for oscilliscope-based testing, you'll likely have to reduce this significantly...
+#define FS  48000 //for oscilliscope-based testing, you'll likely have to reduce this significantly...
 /**** NOT SURE IF SCK SHOULD GO TO BCK OR SCK */
 const i2s_config i2s_config_PCM1502a = {
     FS,     //fs
@@ -81,9 +96,7 @@ int main() {
     
     //populate the input buffer with something, idk???
     for(int i=0; i<STEREO_BUFFER_SIZE;i++) {
-            outsignal[i] = (i%2) ?
-        0x04000000 * sinf( 10*2*M_PI*(float)i / STEREO_BUFFER_SIZE) :
-        0x04000000 * cosf( 10*2*M_PI*(float)i / STEREO_BUFFER_SIZE);
+            outsignal[i] = 0;
        }
 
     // Init GPIO LED
@@ -102,6 +115,13 @@ int main() {
     gpio_set_drive_strength(I2C_SCL, GPIO_DRIVE_STRENGTH_12MA);
     gpio_set_slew_rate(I2C_SDA, GPIO_SLEW_RATE_FAST);
     gpio_set_slew_rate(I2C_SCL, GPIO_SLEW_RATE_FAST);
+
+    adc_init();
+    adc_gpio_init(27);
+    adc_select_input(27 - 26);
+
+    osc1 = init_nco(adc_read(),0);
+    nco_set_amplitude(osc1, 100000000);
     
     
     // PROTOTYPE:       i2s_program_start_synched(pio0, &i2s_config_default, dma_i2s_in_handler, &i2s);
